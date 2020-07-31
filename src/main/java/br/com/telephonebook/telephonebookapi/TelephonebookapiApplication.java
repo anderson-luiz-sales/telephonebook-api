246444030ee3f97c1359c8ba7f923aadeff7ad4f
anderson-luiz-sales <anderson.luiz.sales@gmail.com>
2020-07-31 11:29:45 -0300

    create gitignore

---
+++ src/main/java/br/com/telephonebook/telephonebookapi/TelephonebookapiApplication.java
@@ -0,0 +1,13 @@
+package br.com.telephonebook.telephonebookapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelephonebookapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelephonebookapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/telephonebook/telephonebookapi/TelephonebookapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.telephonebook.telephonebookapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelephonebookapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
